d9d81cac168f2053a54919a1bff0da134982e158
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-10-04 11:05:21 -0300

    ngfor

--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -8,12 +8,20 @@ import {StockService} from './stock.service';
         <ul>
             <li *ngFor="let stock of stocks">{{stock}}</li>
         </ul>
+
+        <hr>
+
+        <ul>
+            <li *ngFor="let stockMarket of stockMarkets">{{stockMarket}}</li>
+        </ul>
         `
 })
 
 export class StocksComponent {
     title = 'List of Stocks';
     stocks = [];
+
+    stockMarkets = ['NYSE', 'NASDAQ', 'EURONEXT', 'HKSE', 'LSE'];
 
     constructor(stockService: StockService) {
         this.stocks = stockService.getStocks();
