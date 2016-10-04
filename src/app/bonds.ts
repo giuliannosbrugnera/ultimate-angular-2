57b17d3711e8c82a612299e73caec579bac0d3df
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-10-04 10:04:19 -0300

    TypeScript basics

---
+++ src/app/bonds.ts
@@ -0,0 +1,21 @@
+import {Security} from './security';
+
+export class Bonds implements Security {
+
+    constructor() {
+
+    }
+
+    getBonds() {
+
+    }
+
+    trade() {
+
+    }
+
+    fairValueCompute() {
+
+    }
+
+}
---
+++ src/app/security.ts
@@ -0,0 +1,6 @@
+export interface Security {
+
+    trade();
+    fairValueCompute();
+
+}
